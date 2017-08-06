f0028c01ecfd9ab82c30ea0ebb8f2d468f1d1434
dnipromv <dnipromv@gmail.com>
2017-08-06 17:02:32 +0200

    finish state refactoring

--- src/states/Finish.js
+++ src/states/Finish.js
@@ -1,22 +1,20 @@
 var finishState = (function () {
   
-  function goToMainScene(){
+  function goToMainScene() {
     this.game.state.start("Menu");
   }
 
   return {
     init: function (score) {
-      this.score = score
+      this.score = score;
     },
     
     create: function () {
-      // init label for displaying final score
-      var label_style = { font: "72px endorregular", 
-                        fill: "#86322e", 
-                        boundsAlignV: "middle",
-                        align: "center" };
-      var label = this.game.add.text( this.game.width * 0.5, this.game.height * 0.5, 
-                                      "Final score: " + this.score, label_style);
+      var labelStyle = { font: "72px endorregular", fill: "#86322e",
+                         boundsAlignV: "middle", align: "center" };
+      
+      var label = this.game.add.text(this.game.width * 0.5, this.game.height * 0.5,
+                                     "Final score: " + this.score, labelStyle);
       label.anchor.set(0.5);
       
       // start single timer to fire state change
