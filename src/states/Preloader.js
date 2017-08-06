64e834b3be7539e3aca89417915aecf83e7aa12b
dnipromv <dnipromv@gmail.com>
2017-08-06 16:21:58 +0200

    Preloader refactoring

--- src/states/Preloader.js
+++ src/states/Preloader.js
@@ -1,15 +1,16 @@
-var loadState = (function() {
+var loadState = (function () {
   
   return {
   
     preload: function () {
-      // load game assets
-      this.game.load.atlasJSONArray( 'basesheet', 'assets/spritesheets/basesheet.png',
-                                     'assets/spritesheets/basesheet.json' );
+      // Image
+      this.game.load.atlasJSONArray('basesheet', 'assets/spritesheets/basesheet.png',
+                                    'assets/spritesheets/basesheet.json');
       this.game.load.spritesheet('buttonsheet', 'assets/spritesheets/buttonsheet.png', 304, 49);
       this.game.load.spritesheet('flamesheet', 'assets/spritesheets/flames.png', 16, 24);
-      this.game.load.image('logo','assets/textures/logo.png');
+      this.game.load.image('logo', 'assets/textures/logo.png');
       
+      // Sounds
       this.game.load.audio('hover_snd', 'assets/sounds/btn_hover.wav');
       this.game.load.audio('press_snd', 'assets/sounds/btn_press.wav');
       this.game.load.audio('chain_1_snd', 'assets/sounds/chain_1.wav');
@@ -21,11 +22,12 @@ var loadState = (function() {
       
       this.game.load.audio('maintheme', 'assets/music/artofescapism__infiltration_of_toy_dungeon.mp3');
       
-      // load data files
+      // Data files
       this.game.load.json('tetrominoes', 'data/tetrominoes.json');
       
-      // force Phaser to preload custom font by creating temporary text object
-      var font_fix = this.game.add.text(0, 0, "fix", {font:"1px endorregular"}); font_fix.destroy();
+      // Force Phaser to preload custom font by creating temporary text object
+      var fontFix = this.game.add.text(0, 0, "fix", {font: "1px endorregular"});
+      fontFix.destroy();
     },
     
     create: function () {
